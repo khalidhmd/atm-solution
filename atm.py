e38fd8fc6c3d4cff84a4da8cd6b004fd13691c8a
khalidhmd <khalidhmd@outlook.com>
2018-02-09 23:36:29 +0200

    Make function withdraw liear. Move check valid operation to separate function

--- atm.py
+++ atm.py
@@ -1,22 +1,31 @@
 """docstring"""
 balance=500
-def withdraw(balance,request):
+
+def is_valid_operation(balance, request):
+    result = False
+    if request < 0:
+        print "Request value must be greater than 0"
+    elif balance>=request:
+        result=True
+    elif request > balance:
+        print "balance is not sufficient"
+    return result
+
+
+def withdraw(balance, request):
     pnv = [100,50,20,10,5]
-    if request > balance:
-        print 'Balance is not sufficient'
-        return
-    
-    for e in pnv:
-        while request >= e:
-            print 'Give ' + str(e)
-            request -= e
-            balance -= e
-    if request > 0:
-        print 'Give ' + str(request)
-    balance -= request
+    if is_valid_operation(balance, request):
+        for e in pnv:
+            while request >= e:
+                print 'Give ' + str(e)
+                request -= e
+                balance -= e
+        if request > 0:
+            print 'Give ' + str(request)
+        balance -= request
     return balance
 
-balance = withdraw(balance,500)
+balance = withdraw(balance,-1)
 print "balance: " + str(balance)
 
             
